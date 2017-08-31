56009aa48e124079d102f273e978431baf020274
A2401013 <A2401013@ulive.pccu.edu.tw>
2017-08-31 14:53:43 +0800

    1

--- WpfCustomer/MainWindow.xaml.cs
+++ WpfCustomer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WpfCustomer
 {
@@ -23,6 +26,37 @@ namespace WpfCustomer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void txtlog_Click(object sender, RoutedEventArgs e)
+        {
+                string strConn = ConfigurationManager.ConnectionStrings["farmsDB"].ConnectionString;
+                string strsql = "login";
+
+                SqlConnection conn = new SqlConnection(strConn);
+                SqlCommand cmd = new SqlCommand(strsql, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("Password", txtPassword.Text);
+
+                SqlParameter pReturnValue = new SqlParameter("@Return_Values", SqlDbType.Int);
+                pReturnValue.Direction = ParameterDirection.ReturnValue;
+                cmd.Parameters.Add(pReturnValue);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                int n = Convert.ToInt32(cmd.Parameters["@Return_Values"].Value);
+                if (n == 1)
+                {
+                    MessageBox.Show("登入成功！");
+                }
+                else
+                {
+                    MessageBox.Show("電子郵件或密碼錯誤！");
+                }
+                conn.Close();
+                conn.Dispose();
         }
     }
 }
