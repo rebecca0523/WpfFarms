db5ad43db2835fabeeb8fa90a09e0c85fbeb98da
ya chin li <selina66@ms68.hinet.net>
2017-08-31 16:33:26 +0800

    edit supplier

--- WpfFarms/MainWindow.xaml.cs
+++ WpfFarms/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfFarmsSupplier;
 
 namespace WpfFarms
 {
@@ -39,7 +40,6 @@ namespace WpfFarms
         private void miSuppliersCRUD_Click(object sender, RoutedEventArgs e)
         {
             SuppliersCRUD SuppliersCRUD = new SuppliersCRUD();
-
             SuppliersCRUD.ShowDialog();
 
         }
--- WpfFarmsSupplier/SuppliersCRUD.xaml.cs
+++ WpfFarmsSupplier/SuppliersCRUD.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfFarms
+namespace WpfFarmsSupplier
 {
     /// <summary>
     /// SuppliersCRUD.xaml 的互動邏輯
