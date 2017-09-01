1fd3c8b95a09ec39e3c55249e06007551ed865e7
ya chin li <selina66@ms68.hinet.net>
2017-09-01 10:27:38 +0800

    edit supplier

--- WpfFarmsSupplier/SuppliersCRUDsingle.xaml.cs
+++ WpfFarmsSupplier/SuppliersCRUDsingle.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,22 @@ namespace WpfFarmsSupplier
     /// <summary>
     /// SuppliersCRUD.xaml 的互動邏輯
     /// </summary>
-    public partial class SuppliersCRUD : Window
+    public partial class SuppliersCRUDsingle : Window
     {
-        public SuppliersCRUD()
+        public SuppliersCRUDsingle()
         {
             InitializeComponent();
         }
+
+        int index = 1;
+        farmsDBEntities farmdb = new farmsDBEntities();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SupplierPanel.DataContext = farmdb.Suppliers.First();
+
+
+        }
+
+        
     }
 }
