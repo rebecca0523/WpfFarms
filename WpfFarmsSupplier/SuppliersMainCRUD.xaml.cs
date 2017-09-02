f5484b94257bec80f966ed75999813d6928e7cc6
ya chin li <selina66@ms68.hinet.net>
2017-09-02 13:39:49 +0800

    .

--- WpfFarmsSupplier/SuppliersMainCRUD.xaml.cs
+++ WpfFarmsSupplier/SuppliersMainCRUD.xaml.cs
@@ -22,14 +22,40 @@ namespace WpfFarmsSupplier
         public SuppliersMainCRUD()
         {
             InitializeComponent();
+            
         }
-
+        System.Windows.Data.CollectionViewSource supplierViewSource;
+        farmsDBEntities context = new farmsDBEntities();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //    System.Windows.Data.CollectionViewSource supplierViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("supplierViewSource")));
+            supplierViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("supplierViewSource")));
 
-            System.Windows.Data.CollectionViewSource supplierViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("supplierViewSource")));
             // 透過設定 CollectionViewSource.Source 屬性載入資料: 
-            // supplierViewSource.Source = [泛用資料來源]
+            supplierViewSource.Source = context.Suppliers.ToList();
+        }
+
+        Supplier SupplierTable = new Supplier();
+        private void cmdAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //SupplierTable.SupplierName = supplierDataGrid.sup
+
+
+            //context.SaveChanges();
+            //supplierViewSource.Source = context.Suppliers.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            supplierViewSource.Source = context.Suppliers.ToList();
+        }
+
+        int suppID;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            supplierViewSource.Source = context.Suppliers.ToList();
         }
     }
 }
