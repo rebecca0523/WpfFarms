cfa765c093031756192b50cd88041f49039a5ee3
ya chin li <selina66@ms68.hinet.net>
2017-09-02 10:46:50 +0800

    .

--- WpfFarmsSupplier/MainWindow.xaml.cs
+++ WpfFarmsSupplier/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,29 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfFarmsSupplier
 {
     /// <summary>
-    /// SuppliersCRUD.xaml 的互動邏輯
+    /// MainWindow.xaml 的互動邏輯
     /// </summary>
-    public partial class SuppliersCRUDsingle : Window
+    public partial class MainWindow : Window
     {
-        public SuppliersCRUDsingle()
+        public MainWindow()
         {
             InitializeComponent();
         }
-
-        int index = 1;
-        farmsDBEntities farmdb = new farmsDBEntities();
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            SupplierPanel.DataContext = farmdb.Suppliers.First();
-
-
-        }
-
-        
     }
 }
