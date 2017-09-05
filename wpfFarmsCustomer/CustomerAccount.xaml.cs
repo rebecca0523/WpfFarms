ac0e294c8f74f7f30f0ecff031533214115579b1
A2401013 <A2401013@ulive.pccu.edu.tw>
2017-09-05 09:33:32 +0800

    OK

--- wpfFarmsCustomer/CustomerAccount.xaml.cs
+++ wpfFarmsCustomer/CustomerAccount.xaml.cs
@@ -11,8 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using WpfFarmsActivity;
-using WpfFarmsSupplier;
 
 namespace wpfFarmsCustomer
 {
@@ -24,19 +22,6 @@ namespace wpfFarmsCustomer
         public CustomerAccount()
         {
             InitializeComponent();
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            ActivityManagement ActivityManagement = new ActivityManagement();
-            ActivityManagement.Show();
-        }
-
-        private void myMenuItemfarmsCRUD_Click(object sender, RoutedEventArgs e)
-        {
-            SuppliersRegister supreg = new SuppliersRegister();
-
-            supreg.Show();
         }
     }
 }
--- wpfFarmsCustomer/DeleteCustomer.xaml.cs
+++ wpfFarmsCustomer/DeleteCustomer.xaml.cs
@@ -23,5 +23,11 @@ namespace wpfFarmsCustomer
         {
             InitializeComponent();
         }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            farmsDBEntities dc = new farmsDBEntities();
+            CustomerInfoDataGrid.ItemsSource = dc.CustomerInfo.ToArray();
+        }
     }
 }
