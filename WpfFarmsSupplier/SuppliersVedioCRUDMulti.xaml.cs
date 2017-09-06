8834f70adaf1485a85b8f8f98ff3ec56a67f3235
ya chin li <selina66@ms68.hinet.net>
2017-09-06 18:42:50 +0800

    .

--- WpfFarmsSupplier/SuppliersVedioCRUDMulti.xaml.cs
+++ WpfFarmsSupplier/SuppliersVedioCRUDMulti.xaml.cs
@@ -32,7 +32,16 @@ namespace WpfFarmsSupplier
 
             System.Windows.Data.CollectionViewSource suppliersVideoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("suppliersVideoViewSource")));
             // 透過設定 CollectionViewSource.Source 屬性載入資料: 
-            suppliersVideoViewSource.Source = context.Suppliers.ToList();
+            suppliersVideoViewSource.Source = context.SuppliersVideos.Where(s=>s.SuppliersID==AllData.CustomerClass.loginSupplierID ).ToList();
+            System.Windows.Data.CollectionViewSource supplierViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("supplierViewSource")));
+            // 透過設定 CollectionViewSource.Source 屬性載入資料: 
+            supplierViewSource.Source = context.Suppliers.Where(s => s.SupplierID == AllData.CustomerClass.loginSupplierID).ToList();
+
+        }
+
+        private void suppliersVideoDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
