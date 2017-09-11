bc24521e06e4ca3257f3a84d8b2df400983dffb2
Rekkuuza <azuukker@gmail.com>
2017-09-11 17:06:07 +0800

    0911

---
+++ WpfFrontpage/FrontpageWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFarms
+{
+    /// <summary>
+    /// FrontpageWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class FrontpageWindow : Window
+    {
+        public FrontpageWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BGmediaElement.Play();
+        }
+
+        private void BGmediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            BGmediaElement.Stop();
+            BGmediaElement.Play();
+        }
+
+      
+    }
+}
