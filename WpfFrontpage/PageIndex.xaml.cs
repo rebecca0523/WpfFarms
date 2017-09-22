23d7401337b2a067ad95962bb6db1a915fc99761
Rekkuuza <azuukker@gmail.com>
2017-09-22 18:18:33 +0800

    0922

---
+++ WpfFrontpage/PageIndex.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFrontpage
+{
+    /// <summary>
+    /// PageIndex.xaml 的互動邏輯
+    /// </summary>
+    public partial class PageIndex : Page
+    {
+        public PageIndex()
+        {
+            InitializeComponent();
+        }
+    }
+
+ 
+}
